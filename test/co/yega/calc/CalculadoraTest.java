fe20dea9c5fad71fd0cc1f20dd90ab33cf4d5f9f
yoinergomez <yoiner.gomez22@gmail.com>
2016-12-28 20:35:10 -0500

    Fallando todas las pruebas
    Fase de fallo

--- test/co/yega/calc/CalculadoraTest.java
+++ test/co/yega/calc/CalculadoraTest.java
@@ -81,7 +81,4 @@ public class CalculadoraTest {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-    
-    
-    
 }
